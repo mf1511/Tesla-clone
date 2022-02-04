c55409fd412aa2e74283d1eaa8633150890db1a1
marin f <marin1511@hotmail.fr>
2022-02-04 14:53:16 +0100

    ajustements header
    ajustements header

---
+++ tesla-clone/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDiToH1vWeGrjgRj5qEDHJ-_0gk1u9VBeU",
+  authDomain: "tesla-clone-1c605.firebaseapp.com",
+  projectId: "tesla-clone-1c605",
+  storageBucket: "tesla-clone-1c605.appspot.com",
+  messagingSenderId: "430292589254",
+  appId: "1:430292589254:web:5ae98046ab2576e84e95c7",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
